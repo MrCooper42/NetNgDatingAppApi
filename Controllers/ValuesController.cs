98293b98ce78427eac5d61b7b9aa85fafd9e31d1
MrCooper42 <mr.cooper42co@gmail.com>
2018-06-06 01:24:40 -0600

    added auth to api

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
@@ -20,6 +22,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
---
+++ DTOs/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.DTOs
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(99, MinimumLength = 8, ErrorMessage = "Password must me more than 8 characters long.")]
+        public string Password { get; set; }
+    }
+}
